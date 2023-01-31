d62b1e7cea69d0242eeb5a00cb50d38136e3fe81
Dmitri5 <dmitriprogram@gmail.com>
2023-01-31 16:39:41 +0700

    Added text with code to the sem_03 task Ex003

--- seminars/sem_03/Ex003.java
+++ seminars/sem_03/Ex003.java
@@ -1,3 +1,6 @@
+// Коллекции. ArrayList.
+// Правильное использование этой коллекции.
+
 package seminars.sem_03;
 
 import java.util.ArrayList;
@@ -5,13 +8,20 @@ import java.util.ArrayList;
 public class Ex003 {
     public static void main(String[] args) {
         ArrayList<Integer> list1 = new ArrayList<Integer>();
-        ArrayList<Integer> list2 = new ArrayList<>();
-        ArrayList<Integer> list3 = new ArrayList<>(10);
+        ArrayList<Integer> list2 = new ArrayList<>(); 
+        // Не пишу <Integer>, т.к. определил тип на первом этапе. Лучше писать всегда.
+        ArrayList<Integer> list3 = new ArrayList<>(10); // (10) - это говорит о том, что
+        // изначально в моей коллекции будет 10 элементов.
         ArrayList<Integer> list4 = new ArrayList<>(list3);
         System.out.println(list3); // []
+        list1.add(1);
         list3.add(123);
+        // list3.add("Hello!"); // Не пройдет компиляцию, т.к. это String, а метод ждет Integer.
         System.out.println(list4); // []
         System.out.println(list3); // [123]
 
     }
 }
+// Плюсы от использования такого метода:
+// Меньше ошибок на этапе компиляции кода;
+// Повторное использование кода.

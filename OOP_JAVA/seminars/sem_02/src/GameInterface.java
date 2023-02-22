eb044f4b6d6b6a928fcc4d532df099a5bd407ea0
Dmitri5 <dmitriprogram@gmail.com>
2023-02-22 19:58:28 +0700

    Added GameInterface.java to the sem_02

---
+++ OOP_JAVA/seminars/sem_02/src/GameInterface.java
@@ -0,0 +1,14 @@
+package OOP_JAVA.seminars.sem_02.src;
+
+public interface GameInterface {
+    void step();
+    String getInfo();
+    
+}
+
+/**
+ * Описание метода: что он возвращает, как называется и какие параметры принимает.
+ *   void step();
+ *   String getInfo();
+ * Тут описанно всего два метода, интерфейса, для этого и нужен он.
+ */

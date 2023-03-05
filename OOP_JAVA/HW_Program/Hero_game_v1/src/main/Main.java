79d5e33002dc2c7689a6d184b466fc72e55726ff
Dmitri5 <dmitriprogram@gmail.com>
2023-03-05 17:35:56 +0700

    Added sem 05

--- OOP_JAVA/HW_Program/Hero_game_v1/src/main/Main.java
+++ OOP_JAVA/HW_Program/Hero_game_v1/src/main/Main.java
@@ -1,108 +1,88 @@
 package OOP_JAVA.HW_Program.Hero_game_v1.src.main;
 
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Crossbowman;
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Farmer;
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Human;
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Monk;
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Names;
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Robber;
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Sniper;
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Spearman;
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Vector2D;
+import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.Witch;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-
-import OOP_JAVA.HW_Program.Hero_game_v1.src.main.Units.*;
-
+import java.util.Random;
+import java.util.Scanner;
 
 
 public class Main {
-    public static void main (String[] args){
+    static final int UNITS = 10;
 
-        Fermer fermer = new Fermer("Миха");
-        System.out.println(fermer.getInfo());
+    public static void main(String[] args) {
+
+        ArrayList<Human> holyTeam = new ArrayList<>();
+        ArrayList<Human> darkTeam = new ArrayList<>();
+        ArrayList<Human> allTeam = new ArrayList<>();
+        Scanner user_input = new Scanner(System.in); 
         
-        ArrayList<Unit> arrayList1 = new ArrayList<>();
-        arrayList1.sort(new Comparator<Unit>() {
-            @Override
-            public int compare(Unit o1, Unit o2) {
-                return o1.getSpeed() - o2.getSpeed();
-            }
-        });
+        createTeam(holyTeam, 0, 1);
+        createTeam(darkTeam, 3, 10);
+        allTeam.addAll(holyTeam);
+        allTeam.addAll(darkTeam);
+        sortTeam(allTeam);
 
-        for (int i = 0; i < 10; i++) { // Создаем 10 случайных персонажей
-            Unit.createArreyUnit1(arrayList1, Unit.setClass());
+        for (Human human: allTeam) {
+            if (holyTeam.contains(human)) human.step(holyTeam, darkTeam);
+            else human.step(darkTeam, holyTeam);
         }
 
-        System.out.println("Команда 1:");
+        while (true){
+            user_input.nextLine();
 
-        for (int i = 0; i < arrayList1.size(); i++) {
-
-            System.out.print(arrayList1.get(i).getInfo());
         }
-
-        ArrayList<Unit> arrayList2 = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) { // Создаем 10 случайных персонажей
-            Unit.createArreyUnit2(arrayList2, Unit.setClass());
-        }
-        System.out.println();
-        System.out.println("Команда 2:");
-
-
-        for (int i = 0; i < arrayList2.size(); i++) {
-            System.out.print(arrayList2.get(i).getInfo());
-        }
-
-        ArrayList arreyAll = new ArrayList<>(arrayList1);
-
-        for (int i = 0; i < arrayList2.size(); i++) {
-            arreyAll.add(arrayList2.get(i));
-        }
-
-        arreyAll.sort(new Comparator<Unit>() {
-            @Override
-            public int compare(Unit o1, Unit o2) {
-                if (o2.getSpeed() == o1.getSpeed()) {
-                    return o1.getHp() - o2.getHp();
-                }
-                return o2.getSpeed() - o1.getSpeed();
-            }
-        });
-
-        System.out.println();
-        System.out.println(arreyAll);
 
     }
+    static void createTeam (ArrayList team, int offset, int posY) {
+        for (int i = 0; i <= UNITS; i++) {
+            int rnd = new Random().nextInt(4)+offset;
+            switch (rnd) {
+                case (0):
+                    team.add(new Sniper(getName(), new Vector2D(i, posY)));
+                    break;
+                case (1):
+                    team.add(new Robber(getName(), new Vector2D(i, posY)));
+                    break;
+                case (2):
+                    team.add(new Witch(getName(), new Vector2D(i, posY)));
+                    break;
+                case (3):
+                    team.add(new Farmer(getName(), new Vector2D(i, posY)));
+                    break;
+                case (4):
+                    team.add(new Crossbowman(getName(), new Vector2D(i, posY)));
+                    break;
+                case (5):
+                    team.add(new Monk(getName(), new Vector2D(i, posY)));
+                    break;
+                case (6):
+                    team.add(new Spearman(getName(), new Vector2D(i, posY)));
+                    break;
+            }
+        }
+    }
+    static void sortTeam (ArrayList<Human> team){
+        team.sort(new Comparator<Human>() {
+            @Override
+            public int compare(Human t0, Human t1) {
+                if (t1.getSpeed() == t0.getSpeed()) return (int) (t1.getHp() - t0.getHp());
+                else  return (int) (t1.getSpeed() - t0.getSpeed());
+            }
+        });
+    }
+    static String getName() {
+        String name = String.valueOf(Names.values()[new Random().nextInt(Names.values().length-1)]);
+        return name;
+    }
 }
-
-
-
-
-/**
- * Задача, 2 семинар:
- * Доделать мою программу, чтобы можно было простым конструктором (только name) создавать экземпляры 
- * персонажей (Крестьянин * Разбойник * Снайпер * Колдун * Копейщик * Арбалетчик * Монах).
- * Добавить интерфейс в котором, будут описанно два метода (getInfo, step).  
- * Метод getInfo - реализовать и переопределить в каждом классе, чтобы он возвращал (Я "name class"!)
- * --------------------------------------------------------------------------------------------------
- * Доработать дерево классов так, чтобы экземпляры классов создавались конструктором с именем 
- * в параметре а остальные параметры инициализировали сами в зависимости от персонажа. 
- * Добавить файл интерфейса в котором описать два метода, void step(); и String getInfo(); 
- * Добавить реализацию интерфейса в ваш коренной абстрактный класс. 
- * Переопределить getInfo так, чтобы он возвращал строки:"Я крестьянин", "Я снайпер" и так далее 
- * в зависимости от типа персонажа. 
- * В основном классе создать список и добавить в него 10 случаных персонажей. 
- * Пройти по списку и вывести строку возвращаемую getInfo.
- * ===================================================================================================
- * Задача, 3 семинар: 
- * Добавить/Исправить.
- * 
- * Атака, защита - должны быть объявлены в abstract class
- * Выстрелы - только у лучника и арбалетчиков. Стрелы заканчиваются и лучник становиться не боеспособным.
- * Крестьянин - за один ход приносит одну стрелу лучнику.
- * Урон - должно быть две цифры damegeMin и damegeMax.
- * hp - должно быть текущее и maxHp.
- * speed - по нему определяется время очередности персонажа.
- * x, y - добавить координаты.
- * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * Д/з:
- * Сделать два списка (команды) по 10:
- * Первый список - или Крестьянин, или Разбойник, или Снайпер, или Колдун;
- * Второй - или Крестьянин, или Копейщик, или Арбалетчик, или Монах.
- * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * Вывести в консоль список персонажей с учетом скорости, чтобы в начале были те у которых 
- * скорость выше внезависимости, от того в каком списки они находятся.
- */

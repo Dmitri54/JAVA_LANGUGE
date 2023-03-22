951697a543bb5136eac134365c57585514422697
Dmitri5 <dmitriprogram@gmail.com>
2023-03-22 12:35:46 +0700

    Corrected the designer, delete int range

--- OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Crossbowman.java
+++ OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Crossbowman.java
@@ -6,25 +6,16 @@ public class Crossbowman extends Arrow { // Арбалетчик
  
 
     public Crossbowman(String name, Vector2D pos) {
-        super(name, 10, 10, 6, 2, 3, 3, 4, pos.x, pos.y, 16, 10 );
+        super(name, 10, 10, 6, 2, 3, 3, 4, pos.x, pos.y, 16 );
 
     }
-
-
 
     @Override
     public void step(ArrayList<Unit> team1, ArrayList<Unit> team2) {super.step(team1, team2);; }
 
     @Override
     public String getInfo() {
-        return "Арбалетчик ";
+        return "Арбалетчик";
     }
 
-    public void shoot(){ // Выстрел
-
-    }
-
-    public void  needHelp(){ // Нужна помощь
-
-    }
 }
--- OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Sniper.java
+++ OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Sniper.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Sniper extends Arrow { // Снайпер
 
     public Sniper(String name, Vector2D pos){
-        super(name, 15, 15, 12, 8, 10, 10, 9, pos.x, pos.y, 15, 10);
+        super(name, 15, 15, 12, 8, 10, 10, 9, pos.x, pos.y, 32);
         
     }
 
@@ -16,7 +16,7 @@ public class Sniper extends Arrow { // Снайпер
 
     @Override
     public String getInfo() {
-        return "Снайпер! ";
+        return "Снайпер";
     }
 
 }

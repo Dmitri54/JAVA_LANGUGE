de54737130de9f05249a29a2acb2bd0e547cec85
Dmitri5 <dmitriprogram@gmail.com>
2023-03-22 12:51:04 +0700

    Corrected designer for Monk

--- OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Monk.java
+++ OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Monk.java
@@ -3,12 +3,12 @@ package OOP_JAVA.HW_Program.Hero_game_v3.src.main.Units;
 public class Monk extends Magic{ // Монах
 
     public Monk(String name, Vector2D pos) {
-        super(name, 30, 30, 12, -4, -4, 7, 5, 5, 5, pos.x, pos.y);
+        super(name, 30, 30, 12, -4, -4, 7, 5, true, pos.x, pos.y);
     }
 
     @Override
     public String getInfo() {
-        return "Монах ";
+        return "Монах";
     }
 
 }

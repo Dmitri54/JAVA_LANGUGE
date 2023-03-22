f0df5648458469d36b6de51f3ff6c172ca4be2f5
Dmitri5 <dmitriprogram@gmail.com>
2023-03-22 12:32:46 +0700

    Corrected metod step and toString for Arrow

--- OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Arrow.java
+++ OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Arrow.java
@@ -4,22 +4,25 @@ import java.util.ArrayList;
 
 public class Arrow extends Unit { // Стрелок
 
-    protected int cartridges, range;
+    protected int cartridges;
 
-    protected Arrow(String name, float hp, int maxHp, int attack, int damageMin, int damageMax, int defense, int speed, int x, int y, int cartridges, int range) {
+    protected Arrow(String name, float hp, int maxHp, int attack, int damageMin, int damageMax, int defense, int speed, int x, int y, int cartridges) {
         super(name, hp, maxHp, attack, damageMin, damageMax, speed, defense, x, y);
         this.cartridges = cartridges;
-        this.range = range;
     }
 
     @Override
     public void step(ArrayList<Unit> team1, ArrayList<Unit> team2 ){
         if (state.equals("Die")) {hp = 0; return;}
         if (state.equals("DIe") || cartridges == 0) return;
-        Unit victim = team2.get(findNearest(team2));
+        int target = findNearest(team2);
 
-        float damage = (victim.defense - attack > 0) ? damageMin : (victim.defense - attack < 0) ? damageMax : ( (damageMax+damageMin) / 2);
-                victim.getDamage(damage);
+        System.out.println(name + " " + getInfo() + " " + team2.get(target).name + " " + team2.get(target).getInfo());
+        
+        float damage = (team2.get(target).defense - attack > 0) ?
+              damageMin : (team2.get(target).defense - attack < 0) ? 
+              damageMax : ( (damageMax+damageMin) / 2);
+        team2.get(target).getDamage(damage);
 
         for (int i = 0; i < team1.size(); i++) {
             if (( team1.get(i).getInfo().equals("Фермер")) && team1.get(i).state.equals("Stand")){
@@ -30,26 +33,14 @@ public class Arrow extends Unit { // Стрелок
         }
         cartridges --;
     }
-
-        // if (state.equals("Die") || cartridges == 0) return; // 3.1 
-
-        // Unit victim = team2.get(findNearest(team2)); // 3.2 
-        // float damage = (victim.defense - attack) > 0 ? damageMin : (victim.defense - attack) < 0 ? damageMax : (damageMin + damageMax) / 2; // 3.3
-        // victim.getDamage(damage);
-        // for (Unit unit : team1) { // 3.4
-        //     if (unit.getInfo().toString().split(":")[0].equals("Фермер") && unit.state.equals("Stand")){
-        //         unit.state = "Busy";
-        //         return;
-        //     }
-        // }
-        // cartridges--; // 3.5 
     
 
     @Override
     public String toString() {
-        return name +
-                " H:" + defense +
-                " D:" + attack +
+        return "🏹" +
+                " H:" + Math.round(hp) +
+                " D:" + defense +
+                " A:" + attack +
                 " Dmg:" + Math.round(Math.abs((damageMin + damageMax) / 2)) +
                 " Shots:" + cartridges + " " +
                 state;

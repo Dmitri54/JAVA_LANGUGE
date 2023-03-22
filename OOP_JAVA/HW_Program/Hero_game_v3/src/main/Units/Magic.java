32ada1a186b1771d890862b886d1e13d84e1978e
Dmitri5 <dmitriprogram@gmail.com>
2023-03-22 12:48:16 +0700

    Corrected designer for Magic

--- OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Magic.java
+++ OOP_JAVA/HW_Program/Hero_game_v3/src/main/Units/Magic.java
@@ -4,27 +4,36 @@ import java.util.ArrayList;
 
 public abstract class Magic extends Unit {
 
-    protected int mana;
-    protected int maxMana;
+    protected boolean magic;
 
     protected Magic(String name, float hp, int maxHp, int attack, int damageMin, int damageMax, int defense, int speed,
-             int mana, int maxMana, int x, int y) {
+             boolean magic, int x, int y) {
         super(name, hp, maxHp, attack, damageMin, damageMax, defense, speed, x, y);
-        this.mana = mana;
-        this.maxMana = maxMana;
+        this.magic = magic;
+
+    }
+    
+    @Override
+    public void step(ArrayList<Unit> team1, ArrayList<Unit> team2) {
+        if (state.equals("Die")) {hp = 0; return;}
+        for (int i = 0; i < team1.size(); i++) {
+            if (team1.get(i).hp < team1.get(i).maxHp){
+                team1.get(i).getDamage(damageMax);
+                System.out.println(name + " " + getInfo() + "  ->  " + team1.get(i).name + " " + team1.get(i).getInfo());
+                break;
+            }
+        }
 
     }
 
     @Override
-    public void step(ArrayList<Unit> team1, ArrayList<Unit> team2) {
-        for (Unit human : team1) {
-            if(human.hp < human.maxHp) {
-                human.getDamage(damageMax);
-                break;
-            }
-            
-        }
+    public String toString() {
+        return "\uD83E\uDDD9" +
+                " H:" + Math.round(hp) +
+                " D:" + defense +
+                " A:" + attack +
+                " Dmg:" + Math.round(Math.abs((damageMin+damageMax)/2)) + " " +
+                state;
     }
-    
     
 }
